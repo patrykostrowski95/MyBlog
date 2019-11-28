76cb4a7772ba73f4956467b8af2dc4a10e5cf16a
patrykostrowski95 <papryk.ostrowski@gmail.com>
2019-11-28 19:48:19 +0100

    patryk

--- blog/forms.py
+++ blog/forms.py
@@ -12,3 +12,4 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('author', 'text')
+
--- blog/models.py
+++ blog/models.py
@@ -46,4 +46,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
-
